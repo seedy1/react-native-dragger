2d972895c19275f38193cd1d5081bf55e26ad4b7
seedy1 <jobe.seedy@gmail.com>
2021-03-31 02:38:39 +0200

    init commit

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Animated } from 'react-native';
+import {PanGestureHandler, State} from "react-native-gesture-handler";
 
-export default function App() {
+export default class App extends React.Component{
+
+  constructor(){
+    super();
+    this.translateX = new Animated.Value(0);
+    this.translateY = new Animated.Value(0);
+    // this.translate = new Animated.ValueXY();
+
+    this.onGestureEvent = Animated.event([
+      {
+        nativeEvent: {
+          translationX: this.translateX,
+          translationY: this.translateY,
+        }
+      }
+    ]);
+
+  
+    // this.onHandlerStateChange = Animated.event([
+    //   {
+    //     nativeEvent: {
+    //       translationX: this.translateX.extractOffset(),
+    //       translationY: this.translateY.extractOffset(),
+    //     }
+    //   }
+    // ]);
+
+
+    
+  }
+
+
+render(){
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <PanGestureHandler onGestureEvent={this.onGestureEvent}>
+           <Animated.View style={[styles.square, {transform: [{ translateX: this.translateX}, { translateY: this.translateY} ] }]} />
+        </PanGestureHandler>
+        <StatusBar style="auto" />
     </View>
-  );
+    );  
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +55,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  square: {
+    backgroundColor: '#2e8b57',
+    width: 75,
+    height: 75,
   },
 });
